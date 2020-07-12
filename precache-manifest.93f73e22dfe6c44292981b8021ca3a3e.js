532270b1cb10d64ee7e222186a0809e377625206
im-adithya <imadithyavardhan@gmail.com>
2020-07-12 11:38:24 +0530

    Updates

---
+++ precache-manifest.93f73e22dfe6c44292981b8021ca3a3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff23d7778be2cc521214338546219a5",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "85025e10ea5e2a2fb28f",
+    "url": "/Pomodoro-Clock/static/js/2.e458bd7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-Clock/static/js/2.e458bd7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b9e475be73ef8016067",
+    "url": "/Pomodoro-Clock/static/js/main.bb9b7089.chunk.js"
+  },
+  {
+    "revision": "4ccfd468b853680e7d1a",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.46c75fa3.js"
+  }
+]);
